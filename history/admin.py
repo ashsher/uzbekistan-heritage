21b8a6f5508d24e20d9dc4d912fbd43c3960af7a
Ashraf Shermatov <w1980920@my.westminster.ac.uk>
2026-02-11 09:19:14 +0500

    Second commit: Added database models

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import TimePeriod, HistoricalFigure, HistoricalSite
 
-# Register your models here.
+
+@admin.register(TimePeriod)
+class TimePeriodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'start_year', 'end_year']
+    search_fields = ['name', 'description']
+    list_filter = ['start_year']
+
+
+@admin.register(HistoricalFigure)
+class HistoricalFigureAdmin(admin.ModelAdmin):
+    list_display = ['name', 'role', 'time_period', 'birth_year', 'death_year']
+    search_fields = ['name', 'biography']
+    list_filter = ['role', 'time_period']
+    autocomplete_fields = ['time_period']
+
+
+@admin.register(HistoricalSite)
+class HistoricalSiteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city', 'built_year']
+    search_fields = ['name', 'description']
+    list_filter = ['city', 'time_periods']
+    filter_horizontal = ['time_periods', 'related_figures']
--- history/models.py
+++ history/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TimePeriod(models.Model):
+    """Represents historical periods like empires, khanates, eras"""
+    name = models.CharField(max_length=200)
+    start_year = models.IntegerField()
+    end_year = models.IntegerField(null=True, blank=True)  # null if ongoing
+    description = models.TextField()
+    image = models.ImageField(upload_to='periods/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['start_year']
+    
+    def __str__(self):
+        return self.name
+
+
+class HistoricalFigure(models.Model):
+    """Represents important historical individuals"""
+    ROLE_CHOICES = [
+        ('ruler', 'Ruler/Khan'),
+        ('scientist', 'Scientist/Scholar'),
+        ('poet', 'Poet/Writer'),
+        ('warrior', 'Military Leader'),
+        ('architect', 'Architect/Builder'),
+        ('other', 'Other'),
+    ]
+    
+    name = models.CharField(max_length=200)
+    birth_year = models.IntegerField(null=True, blank=True)
+    death_year = models.IntegerField(null=True, blank=True)
+    biography = models.TextField()
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='other')
+    time_period = models.ForeignKey(TimePeriod, on_delete=models.CASCADE, related_name='figures')
+    image = models.ImageField(upload_to='figures/', null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['birth_year']
+    
+    def __str__(self):
+        return self.name
+
+
+class HistoricalSite(models.Model):
+    """Represents historical places and monuments"""
+    CITY_CHOICES = [
+        ('tashkent', 'Tashkent'),
+        ('samarkand', 'Samarkand'),
+        ('bukhara', 'Bukhara'),
+        ('khiva', 'Khiva'),
+        ('shahrisabz', 'Shahrisabz'),
+        ('termez', 'Termez'),
+        ('other', 'Other'),
+    ]
+    
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=20, choices=CITY_CHOICES)
+    built_year = models.IntegerField(null=True, blank=True)
+    description = models.TextField()
+    time_periods = models.ManyToManyField(TimePeriod, related_name='sites', blank=True)
+    related_figures = models.ManyToManyField(HistoricalFigure, related_name='sites', blank=True)
+    image = models.ImageField(upload_to='sites/', null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return f"{self.name} ({self.city})"
